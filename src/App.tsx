d1674e8508bab93fe3423c8dd84b37b25d2259c8
Tommy <tommyphan2004.tp@gmail.com>
2022-06-30 12:45:28 +0200

    Added Routes and MainView.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Header } from './components/layout/Header';
-import { Banner } from './components/layout/Banner';
 import { AddForm } from './components/AddForm/AddForm';
+import { MainView } from './views/MainView';
 
 export const App = () => {
     return <>
-        <Header/>
-        <Banner/>
-        {/*<Routes>*/}
-        {/*    <Route path="/add" element={<AddForm/>}/>*/}
-        {/*</Routes>*/}
+        <Routes>
+            <Route path="/" element={<MainView/>}/>
+            <Route path="/add" element={<AddForm/>}/>
+        </Routes>
     </>;
 };
 
---
+++ src/views/MainView.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Header } from '../components/layout/Header';
+import { Banner } from '../components/layout/Banner';
+
+export const MainView = () => (
+    <>
+        <Header/>
+        <Banner/>
+    </>
+);
