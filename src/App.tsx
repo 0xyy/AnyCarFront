a16a8ad687f75496fce7c8644138e27295a3c4a9
Tommy <tommyphan2004.tp@gmail.com>
2022-07-10 13:47:27 +0200

    Added search context to the app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Header } from './components/layout/Header';
+import { SearchContext } from './contexts/search.context';
+import { Header } from './components/layout/Header/Header';
 import { MainView } from './views/MainView';
 import { AddFormView } from './views/AddFormView';
+import { SingleCarView } from './views/SingleCarView';
 import { NotFoundView } from './views/NotFoundView';
 
 export const App = () => {
-    return <>
-        <Header/>
-        <Routes>
-            <Route path="/" element={<MainView/>}/>
-            <Route path="/add" element={<AddFormView/>}/>
-            <Route path="/*" element={<NotFoundView/>}/>
-        </Routes>
-    </>;
+    const [search, setSearch] = useState('');
+
+    return (
+        <SearchContext.Provider value={{search, setSearch}}>
+            <Header/>
+            <Routes>
+                <Route path="/" element={<MainView/>}/>
+                <Route path="/add" element={<AddFormView/>}/>
+                <Route path="/car/:id" element={<SingleCarView/>}/>
+                <Route path="/*" element={<NotFoundView/>}/>
+            </Routes>
+        </SearchContext.Provider>
+    );
 };
 
